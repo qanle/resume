5c361a6f90f3e60e3897a53cae2dc2b3c233d51a
kevin <kevin@arrowhitech.co>
2021-12-23 15:21:56 +0700

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,8 @@ AOS.init({
 });
 
 // Add your javascript here
+function sendMail(){
+  var subject = $("#subject").val();
+  var body = $("#message").val();
+  window.open('mailto:qan.soft@gmail.com?subject='+subject+'&body='+body);
+}
